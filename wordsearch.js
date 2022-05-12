0688a565adc270f54b3f8e9a6d4540dd42f8b0c3
ttbinh139 <ttbinh139@gmail.com>
2022-05-12 02:11:33 +0000

    Pair programming with Rafael

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,23 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    let bword = word.split("").reverse().join("");
+    const allArraysJoin = letters.map(ls => ls.join('')) // joining all rows into an array
+    for (let i = 0; i < letters[0].length; i++) { // loops through each column
+        let tempArr = []; // creates a temporary array that's going to be used in the next loop below
+        for (let j = 0; j < letters.length; j++) { // loops through each row
+            tempArr.push(letters[j][i]); //push every letter of specific column into the temapArr
+        }
+        allArraysJoin.push(tempArr.join("")); // push the new arrays into the existing allArraysJoin
     }
+    console.log(allArraysJoin);
+    for (line of allArraysJoin) {
+        if (line.includes(word)) {
+            return true;
+        }
+        if (line.includes(bword)) {
+            return true;
+        }
+    }
+    return false;
 }
 
 module.exports = wordSearch
